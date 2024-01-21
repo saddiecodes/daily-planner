b6763418816648c7bab702daa249d36f64e1a66f
saddiecodes <hannahbarrie1999@gmail.com>
2024-01-21 22:10:45 +0000

    added functions with the bug in the code solved

--- js/planner.js
+++ js/planner.js
@@ -1,21 +1,18 @@
-var currentHour = dayjs().hour() 
-console.log(currentHour) 
+var currentHour = dayjs().hour();
+console.log(currentHour);
+
 // Wait for the document to be ready before executing any code
-$(document).ready(function() {
-    // Display the current day at the top of the calendar 
+$(document).ready(function () {
+    // Display the current day at the top of the calendar
     $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 
-    // Function to colour-code timeblocks based on past present or future
+    // Function to colour-code timeblocks based on past, present, or future
     function updateColors() {
-        $(".time-block").each(function() {
-            varhour = parseInt($(this).attr("data-hour"));
-            if (hour <currentHour) {
-                
-            }
-        }
+        $(".time-block").each(function () {
+            var hour = parseInt($(this).attr("data-hour"));
+            if (hour < currentHour) {
+                $(this).addClass("past");
+            } 
+        });
     }
-
-
-}
-  
-  
+});
