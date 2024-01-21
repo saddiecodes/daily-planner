847577f35920669050e28827b2858fc2156831fc
saddiecodes <hannahbarrie1999@gmail.com>
2024-01-21 21:37:03 +0000

    displaying time at top of the page currently not working to debug

--- js/planner.js
+++ js/planner.js
@@ -1,4 +1,10 @@
 var currentHour = dayjs().hour() 
 console.log(currentHour) 
+// Wait for the document to be ready before executing any code
+$(document).ready(function() {
+    // Display the current day at the top of the calendar 
+    $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 
+}
+  
   
